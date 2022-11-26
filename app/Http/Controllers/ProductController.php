1f9ee3aa1a18402374fffe05125c74a94151c14c
Nour Alkinj <noor22w2@gmail.com>
2022-11-26 22:22:39 +0200

    b

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -31,48 +31,35 @@ class ProductController extends Controller
             ['message' => __('common.store'), 'type' => 'Success']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Product $product)
+
+    public function show( $id)
     {
-        //
+//        $parameters = ['id' => $id];
+          $product = Product::find($id);
+//          $this->callActivityMethod('products', 'show', $parameters);
+          return $product;
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Product $product)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateProductRequest  $request
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(UpdateProductRequest $request, Product $product)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
+    public function delete( $id)
     {
-        //
+//        $parameters = ['id' => $id];
+        $product = Product::find($id);
+        $product->delete();
+//        $this->callActivityMethod('products', 'delete', $parameters);
+        return redirect()->route('product.index')->with(['message' => __('common.delete'), 'type' => 'Success',]);
+
     }
 }
