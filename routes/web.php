b7e866eb7bc1af5c2f9e3714fe7575022998e76e
Nour Alkinj <noor22w2@gmail.com>
2022-12-03 23:31:33 +0200

    'nour'

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,10 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::group(['namespace' => 'product', 'prefix' => 'product'], function () {
+    Route::get('index', [ProductController::class, 'index'])->name('product.index');
+
+
+});
